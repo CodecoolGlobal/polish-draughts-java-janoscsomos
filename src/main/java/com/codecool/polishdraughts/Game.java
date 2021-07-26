af4177f817231da9124a8d23290921ae84874cc3
Sztomi8 <szilagyitamas94@gmail.com>
2021-07-26 14:45:17 +0200

    mob programming, impelent the board

--- src/main/java/com/codecool/polishdraughts/Game.java
+++ src/main/java/com/codecool/polishdraughts/Game.java
@@ -1,6 +1,4 @@
 package com.codecool.polishdraughts;
-
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Game {
@@ -10,6 +8,32 @@ public class Game {
         int coordinates = boardSize.nextInt();
         Board board = new Board();
         board.initBoard(coordinates);
-        System.out.println(Arrays.deepToString(board.getBoard()));
+        printBoard(board.getBoard(),coordinates);
+    }
+
+    public static void printBoard(String[][] board,int coordinates) {
+        StringBuilder visualBoard = new StringBuilder();
+        int rowNumber = 1;
+        final String alphabetString = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+        visualBoard.append("   ");
+        for (int index = 0; index<coordinates; index++){
+            visualBoard.append("   ").append(alphabetString.toCharArray()[index]);
+        }
+        visualBoard.append("\n");
+        for (String[] row : board) {
+
+            visualBoard.append(rowNumber);
+            if (rowNumber < 10) {
+                visualBoard.append("   ");
+            } else {
+                visualBoard.append("  ");
+            }
+            for (String element : row) {
+                visualBoard.append("|_").append(element).append("_");
+            }
+            visualBoard.append("|\n");
+            rowNumber++;
+        }
+        System.out.println(visualBoard);
     }
 }
